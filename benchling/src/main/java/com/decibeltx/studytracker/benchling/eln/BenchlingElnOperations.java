28bec5f07e5600e8ebb0b4491da9f7189580bb05
simoond <david@davidtwomey.com>
2020-07-17 12:05:20 -0400

    Benchling (#25)
    * Addresses issue #5.
    
    * change study ID starting number to 201
    
    * added logo
    
    * added Benchling module
    
    * added benchling module
    
    * Added Benchling Integration
    
    * fixed getLabel
    
    * Fix regex
    Clean up code
    
    * added cleanup of code
    
    * added fix for legacy notebook URL
    
    * Addressed PR comments and fixed legacy ELN notebook creation.
    
    Co-authored-by: woemler <woemler@decibeltx.com>

---
+++ benchling/src/main/java/com/decibeltx/studytracker/benchling/eln/BenchlingElnOperations.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2020 the original author or authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.decibeltx.studytracker.benchling.eln;
+
+import com.decibeltx.studytracker.benchling.eln.entities.BenchlingNotebookEntry;
+
+import java.util.List;
+
+public interface BenchlingElnOperations {
+
+  /**
+   * Gets an entity by it's ID.
+   *
+   * @param entityId
+   * @return
+   */
+ BenchlingNotebookEntry findEntityById(String entityId);
+
+  List<BenchlingNotebookEntry> findEntityChildren(String entityId);
+
+  /**
+   * Creates a new study folder in a program directory.
+   *
+   * @param studyName
+   * @param programEntityId
+   * @return
+   */
+  String createStudyFolder(String studyName, String programEntityId);
+
+}
--- core/src/main/java/com/decibeltx/studytracker/core/events/listener/NewStudyNotebookListener.java
+++ core/src/main/java/com/decibeltx/studytracker/core/events/listener/NewStudyNotebookListener.java
@@ -19,21 +19,61 @@ package com.decibeltx.studytracker.core.events.listener;
 import com.decibeltx.studytracker.core.events.type.EventType;
 import com.decibeltx.studytracker.core.events.type.StudyEvent;
 import com.decibeltx.studytracker.core.model.Study;
+import com.decibeltx.studytracker.core.notebook.NotebookEntry;
+import com.decibeltx.studytracker.core.notebook.NotebookService;
+import com.decibeltx.studytracker.core.repository.StudyRepository;
+import java.util.Date;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class NewStudyNotebookListener {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(NewStudyFolderListener.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(NewStudyNotebookListener.class);
+
+  @Autowired(required = false)
+  private NotebookService notebookService;
+
+  @Autowired
+  private StudyRepository studyRepository;
 
   @EventListener
   public void onApplicationEvent(StudyEvent studyEvent) {
     if (studyEvent.getEventType().equals(EventType.NEW_STUDY)) {
       Study study = studyEvent.getStudy();
       LOGGER.warn(String.format("TODO: Creating ELN entry for study: %s", study.getCode()));
+
+      if (notebookService != null) {
+
+        if (study.isLegacy()) {
+          LOGGER.warn(String.format("Legacy Study : %s", study.getCode()));
+          NotebookEntry notebookEntry = study.getNotebookEntry();
+          notebookEntry.setLabel("Benchling");
+          study.setNotebookEntry(notebookEntry);
+          study.setUpdatedAt(new Date());
+          studyRepository.save(study);
+        } else {
+          try {
+            NotebookEntry notebookEntry = notebookService.createStudyEntry(study);
+            study.setNotebookEntry(notebookEntry);
+            study.setUpdatedAt(new Date());
+            studyRepository.save(study);
+          } catch (Exception e) {
+            e.printStackTrace();
+            LOGGER.error("Failed to create notebook entry for study: " + study.getCode());
+          }
+        }
+      } else {
+        LOGGER.warn(String.format("No notebook mode selected: "));
+
+      }
+
+    } else {
+      LOGGER.warn(String.format("Not New Study"));
     }
   }
 }
+
