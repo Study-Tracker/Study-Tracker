601ea526fc777ce5de9ea51a166ce28b1400ef0b
Will Oemler <willoemler@gmail.com>
2022-06-02 13:46:33 -0400

    Added database migration scripts. Added support for external SAML keystore files.

---
+++ scripts/clean_db.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Test if run directory is correct
+CONF_FILE=$(pwd)/flyway.conf
+if [ ! -f "$(pwd)/pom.xml" ]; then
+    echo "Please run this script from the root directory of the project."
+    exit 1
+fi
+if [ ! -f "${CONF_FILE}" ]; then
+    echo "flyway.conf file does not exist."
+    exit 1
+fi
+
+read -p "Are you sure you want to clean the database? All data will be lost and migrations will be reapplied. (y/n) " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    echo "Cleaning database..."
+    mvn flyway:clean flyway:migrate -Dflyway.configFiles=${CONF_FILE}
+    echo "Done."
+fi
