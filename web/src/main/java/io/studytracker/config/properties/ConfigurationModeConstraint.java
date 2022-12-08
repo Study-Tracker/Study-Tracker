27fbd459398fe7c1165e40a292e2580e32afb3fd
woemler <woemler@decibeltx.com>
2022-12-07 20:42:37 -0500

    Code quality

--- web/src/main/java/io/studytracker/config/properties/ConfigurationModeConstraint.java
+++ web/src/main/java/io/studytracker/config/properties/ConfigurationModeConstraint.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.studytracker.config.properties;
 
 import io.studytracker.config.properties.ConfigurationModeConstraint.ConfigurationModeValidator;
@@ -39,7 +55,7 @@ public @interface ConfigurationModeConstraint {
         valid = allowEmpty;
       } else {
         for (String option : options) {
-          if (option.equalsIgnoreCase(s.toLowerCase())) {
+          if (option.equalsIgnoreCase(s)) {
             valid = true;
             break;
           }
